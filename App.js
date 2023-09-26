75d59df4625c4cc2f9b316ad0dd4191348078079
Heberton <hebertontecno@hotmail.com>
2023-09-26 16:55:50 -0300

    inicio do projeto

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity><Text>Segunda</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Terça</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Quarta</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Quinta</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Sexta</Text></TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
